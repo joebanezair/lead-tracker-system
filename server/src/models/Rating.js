1cc3b2d5a2c90ab50b132f89c9602ed51aaad7c1
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2026-09-25 16:37:15 +0800

    Scaffold server/src/models/Rating.js

---
+++ server/src/models/Rating.js
@@ -0,0 +1,1 @@
+import mongoose from'mongoose';const schema=new mongoose.Schema({leadId:{type:mongoose.Schema.Types.ObjectId,ref:'Lead',required:true},userId:{type:mongoose.Schema.Types.ObjectId,required:true},rating:{type:Number,min:1,max:5,required:true}},{timestamps:true});schema.index({leadId:1,userId:1},{unique:true});export default mongoose.model('Rating',schema);
