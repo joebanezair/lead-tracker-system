d452762d421b2c19cdcceddef7a8829862fa0e22
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2026-09-25 17:10:26 +0800

    Format source code without functional changes

--- client/src/components/import/FileDropzone.jsx
+++ client/src/components/import/FileDropzone.jsx
@@ -1,1 +1,12 @@
-import{FiUploadCloud}from'react-icons/fi';export default function FileDropzone(){return <div className="drop"><FiUploadCloud size={38}/><b>Drag XLSX, XLS or CSV here</b><span>Flexible column mapping supported · up to 100,000 rows</span><button>Choose file</button></div>}
+import { FiUploadCloud } from 'react-icons/fi';
+
+export default function FileDropzone() {
+  return (
+    <div className="drop">
+      <FiUploadCloud size={38} />
+      <b>Drag XLSX, XLS or CSV here</b>
+      <span>Flexible column mapping supported · up to 100,000 rows</span>
+      <button>Choose file</button>
+    </div>
+  );
+}
