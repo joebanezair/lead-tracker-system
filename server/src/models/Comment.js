3e4be5e5dc655c18818286c9138d44944b007746
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2026-09-25 17:11:07 +0800

    Format source code without functional changes

--- server/src/models/Comment.js
+++ server/src/models/Comment.js
@@ -1,1 +1,12 @@
-import mongoose from'mongoose';const schema=new mongoose.Schema({leadId:{type:mongoose.Schema.Types.ObjectId,ref:'Lead',index:true},userId:{type:mongoose.Schema.Types.ObjectId,index:true},content:{type:String,required:true,trim:true,maxlength:4000}},{timestamps:true});export default mongoose.model('Comment',schema);
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema(
+  {
+    leadId: { type: mongoose.Schema.Types.ObjectId, ref: 'Lead', index: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, index: true },
+    content: { type: String, required: true, trim: true, maxlength: 4000 }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Comment', schema);
